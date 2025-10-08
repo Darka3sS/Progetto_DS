540d21637bd63b9ef7acb493627f02355f4550fc
Marcos Del Pio <mrcdelpio@gmail.com>
2025-10-08 20:24:39 +0200

    modifiche

---
+++ slides_script.bat
@@ -0,0 +1,4 @@
+
+cd C:\Users\Marcos Del Pio\Desktop\Progetti\Progetto_DS
+
+python -m nbconvert Presentazione.ipynb  --to slides --SlidesExporter.reveal_number='c/t' --SlidesExporter.reveal_scroll=True --no-input
